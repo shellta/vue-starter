3988b3047afa1ba11844fe1d68791111a8db76b9
Shawn Zhan <javascript@live.it>
2018-03-16 16:55:49 +0800

    fix styles.

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,12 +5,12 @@ var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/main.ts'
+    app: './src/main.ts',
   },
   output: {
     path: path.resolve(__dirname, './dist/'),
     publicPath: '/',
-    filename: 'static/js/[name].[chunkhash].js'
+    filename: 'static/js/[name].[chunkhash].js',
   },
   module: {
     rules: [
@@ -19,96 +19,95 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            ts: 'ts-loader!tslint-loader',          
+            ts: 'ts-loader!tslint-loader',
             scss: 'vue-style-loader!css-loader!sass-loader',
             sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-          }
-        }
+          },
+        },
       },
       {
         test: /\.tsx?$/,
         loader: 'tslint-loader',
         exclude: /node_modules/,
-        enforce: 'pre'
+        enforce: 'pre',
       },
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
         options: {
-          appendTsSuffixTo: [/\.vue$/]
-        }
+          appendTsSuffixTo: [/\.vue$/],
+        },
       },
       {
-        test: /\.css$/, 
+        test: /\.css$/,
         use: ExtractTextPlugin.extract({
             use: 'css-loader',
-            fallback: 'style-loader'
-        })
+            fallback: 'style-loader',
+        }),
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: 'static/images/[name].[ext]'
-        }
+          name: 'static/images/[name].[ext]',
+        },
       },
       {
         test: /\.(eot|woff|woff2|ttf)$/,
         loader: 'url-loader',
         options: {
           name: 'static/fonts/[name].[ext]',
-          limit: 30000
-        }
+          limit: 30000,
+        },
       }
     ]
   },
   resolve: {
     extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
+      'vue$': 'vue/dist/vue.esm.js',
+    },
   },
   devServer: {
     historyApiFallback: true,
     noInfo: true,
     host: '127.0.0.1',
     port: 8080,
-    contentBase: './dist/'
+    contentBase: './dist/',
   },
   performance: {
-    hints: false
+    hints: false,
   },
   devtool: false,
   plugins: [
-    new ExtractTextPlugin("static/css/app.[chunkhash].css"),
+    new ExtractTextPlugin("static/css/[name].[chunkhash].css"),
     new htmlWebpackPlugin({
       title: '',
-      template: './src/static/template.html'
-    })
-  ]
-  // stats: "errors-only"  
+      template: './src/static/template.html',
+    }),
+  ],
 }
 
 module.exports.plugins = (module.exports.plugins || []).concat([
-  // new webpack.optimize.CommonsChunkPlugin({ 
-  //   name: 'vendor' 
+  // new webpack.optimize.CommonsChunkPlugin({
+  //   name: 'vendor',
   // }),
   new webpack.DefinePlugin({
     'process.env': {
-      NODE_ENV: '"production"'
-    }
+      NODE_ENV: '"production"',
+    },
   }),
   new webpack.optimize.UglifyJsPlugin({
     sourceMap: false,
     compress: {
-      warnings: false
+      warnings: false,
     },
     output: {
-      comments: false
-    }
+      comments: false,
+    },
   }),
   new webpack.LoaderOptionsPlugin({
-    minimize: true
-  })
+    minimize: true,
+  }),
 ])
